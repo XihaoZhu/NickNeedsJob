3f3ce37fe70e95fb1ed4776fc40111dc3c9cc9e3
Xihao Zhu <xihao_zhu2021@163.com>
2024-01-10 12:35:34 +0000

    description in title

--- src/pages/entrance/title/index.js
+++ src/pages/entrance/title/index.js
@@ -8,6 +8,14 @@ const Title = () => {
       <img src={require('src/assets/images/NNJ_logo.png')} className='logoImg'></img>
       <span>Nick Needs Jobs</span>
       </div>
+      <div className='description'>
+      What you are viewing now is a website produced by Nick himself, aiming to find a <br/>
+      position in the front-end field. This website is built with a React structure <br/>
+      and is designed for desktop devices. Click <a href=''>here</a> to view the <br/>
+      website built with Vue for mobile devices. Click <br/>
+      <a href=''>here</a> to download the files in the <br/>
+      developing environment.
+      </div>
     </div>
   )
 }
