60212f5927781d5eb022f957c337ef68492b08de
Xihao Zhu <xihao_zhu2021@163.com>
2024-01-11 11:46:51 +0000

    experience parts mainly finished

--- src/pages/entrance/experience/index.js
+++ src/pages/entrance/experience/index.js
@@ -2,49 +2,101 @@ import './index.scss'
 import React, { useState } from 'react'
 
 const Experience = () => {
+  // control the lights
   const [isOn, setIsOn] = useState(0)
   function turnLightOn (whichOne) {
     setIsOn(whichOne)
   }
 
+  // control the show of key words
+  const [whichKeyword, setWhichKeyword] = useState([0, 0, 0, 0, 0])
+  function selectKeyWord (num) {
+    const arry = [...whichKeyword]
+    arry.splice(num, 1, 1)
+    setWhichKeyword(arry)
+  }
+  function unSelectKeyword () {
+    setWhichKeyword([0, 0, 0, 0, 0])
+    console.log(whichKeyword)
+  }
+
   return (
     <div className='Experience'>
-      <div className='left' onClick={() => turnLightOn(1)}>
-        <img src={require('../../../assets/images/bulb.png')} style={{ top: isOn === 1 ? '47vw' : '46vw' }}></img>
-          <div className='EducationHistory' style={{ opacity: isOn === 1 ? 0.8 : 0 }}>
-           {isOn === 1 && <>
-              <div>2022.9 – 2023.12<br/>
-              <span className='master'>MSc Aerospace Engineering</span> University  of Manchester</div><br/>
-              <div>2018.9 – 2022.5<br/>
-              <span className='undergranduate'>B. Eng Aerospace Engineering</span> University of Nottingham, Ningbo, China</div>
-            </>
-            }
-          </div>
-          {isOn === 1 ||
-            <div className='EduTitle'>
-              Education <br/>
-              History
-            </div>}
+      {/* left side */}
+      <div className='left'>
+        {/* lights on or off */}
+        <img onClick={() => turnLightOn(1)} src={require('../../../assets/images/bulb.png')} style={{ top: isOn === 1 ? '47vw' : '46vw' }}></img>
+        {/* when the other light on and hovered on key words */}
+        <div className='extraIntern' style={{ opacity: whichKeyword[2] }}>
+          Telemarketing:<br/>
+          A part-time job as a telemarketing in a children&apos;s art training institution.
+          Not doing scam! But to invite parents taking their children to try the free trial lesson.
+          Sometimes leafleting in street too.
+          Learnt skills how to communicate with custormers and advertise during the work.
+        </div>
+        <div className='extraIntern' style={{ opacity: whichKeyword[3] }}>
+          In a barber shop:<br/>
+          Worked in barber shop as an apprentice for a month. Learnt how to wash hair and basic massage involved in.
+          Trained my patience <br/>
+          ...and finger flexibility? XD
+        </div>
+        <div className='extraIntern' style={{ opacity: whichKeyword[4] }}>
+          Had a two months internship in this company.
+          One of the two main roles is operating machine, communicate with remote team to fine-tune it. The other is designing and built a pair of high-arch models and a pair of flat-foot models for testing the machine.
+          Leant communication between different teams and practised my modeling skill.
+        </div>
+        {/* main content */}
+        <div className='EducationHistory' style={{ opacity: isOn === 1 ? 0.8 : 0 }}>
+         {isOn === 1 && <>
+            <div>2018.9 – 2022.5<br/>
+            <span className='undergranduate' onMouseEnter={() => selectKeyWord(0)} onMouseLeave={() => unSelectKeyword()}>B. Eng Aerospace Engineering</span> University of Nottingham, Ningbo, China</div>
+            <div>2022.9 – 2023.12<br/>
+            <span className='master' onMouseEnter={() => selectKeyWord(1)} onMouseLeave={() => unSelectKeyword()}>MSc Aerospace Engineering</span> University  of Manchester</div><br/>
+          </>
+          }
+        </div>
+        {isOn === 1 ||
+          <div className='EduTitle'>
+            Education <br/>
+            History
+          </div>}
       </div>
       <div className='middle'></div>
-      <div className='right' onClick={() => turnLightOn(2)}>
-        <img src={require('../../../assets/images/bulb.png')} style={{ top: isOn === 2 ? '47vw' : '46vw' }}></img>
-          <div className='InternHistory' style={{ opacity: isOn === 2 ? 0.8 : 0 }}>
-           {isOn === 2 && <>
-            <div>2018.7 – 2018.8<br/>
-            Part-time job <span className='telemarketing'>Telemarketing</span></div>
-            <div>2019.7 – 2019.8<br/>
-            Part-time job <span className='barber'>barber</span> shop</div>
-            <div>2021.7 – 2021.8<br/>
-            <span className='internship'>Internship</span> in 宁波三体职能科技有限公司, China</div>
-            </>
-            }
-          </div>
-          {isOn === 2 ||
-            <div className='InternTitle'>
-              Internship <br/>
-              History
-            </div>}
+      {/* right side */}
+      <div className='right'>
+        {/* light on or off */}
+        <img onClick={() => turnLightOn(2)} src={require('../../../assets/images/bulb.png')} style={{ top: isOn === 2 ? '47vw' : '46vw' }}></img>
+        {/* when the other light on and hovered on key words */}
+        <div className='extraIntern' style={{ opacity: whichKeyword[0] }}>
+          Undergranduate:<br/>
+          Main courses were around mathmatics and physics.
+          Logical thinking was trianed, improved myself in how to learn.
+          Besides the theoritical learning, also got chances leanring relative skills.
+          (Check in Core Skills - others for more information)
+        </div>
+        <div className='extraIntern' style={{ opacity: whichKeyword[1] }}>
+          Master:<br/>
+          An extension for my undergranduate study.
+          More relative skills trained.
+          (Also check in Core Skills - others for more information)
+        </div>
+        {/* main content */}
+        <div className='InternHistory' style={{ opacity: isOn === 2 ? 0.8 : 0 }}>
+         {isOn === 2 && <>
+          <div>2018.7 – 2018.8<br/>
+          Part-time job <span className='telemarketing' onMouseEnter={() => selectKeyWord(2)} onMouseLeave={() => unSelectKeyword()}>Telemarketing</span></div>
+          <div>2019.7 – 2019.8<br/>
+          Part-time job <span className='barber' onMouseEnter={() => selectKeyWord(3)} onMouseLeave={() => unSelectKeyword()}>barber</span> shop</div>
+          <div>2021.7 – 2021.8<br/>
+          <span className='internship' onMouseEnter={() => selectKeyWord(4)} onMouseLeave={() => unSelectKeyword()}>Internship</span> in 宁波三体职能科技有限公司, China</div>
+          </>
+          }
+        </div>
+        {isOn === 2 ||
+          <div className='InternTitle'>
+            Internship <br/>
+            History
+          </div>}
       </div>
     </div>
   )
