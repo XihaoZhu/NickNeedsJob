29f84d875fb90a8c8d0d6c1df8df965c1dfc4e1c
Xihao Zhu <xihao_zhu2021@163.com>
2024-01-15 15:59:37 +0000

    leftside structure done

--- src/pages/Blender/LeftList/index.js
+++ src/pages/Blender/LeftList/index.js
@@ -2,9 +2,23 @@ import React from 'react'
 import './index.scss'
 
 const List = () => {
+  const fromCourse = [
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {}
+  ]
   return (
     <div className="LeftList">
-      我是list
+      {fromCourse.map((item, index) => {
+        return (
+      <div key={index} className='example' style={{ backgroundImage: `url(${require('../../../assets/Blender/ThreeGuys.png')})`, backgroundSize: 'cover' }}>
+      </div>)
+      })}
     </div>
   )
 }
