71da48282133adb352e08effe41f1c8726ac04a4
Xihao Zhu <xihao_zhu2021@163.com>
2024-01-15 16:24:37 +0000

    blender page structure done

--- src/pages/Blender/LeftList/index.js
+++ src/pages/Blender/LeftList/index.js
@@ -3,20 +3,20 @@ import './index.scss'
 
 const List = () => {
   const fromCourse = [
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {}
+    require('../../../assets/Blender/example1.png'),
+    require('../../../assets/Blender/example2.png'),
+    require('../../../assets/Blender/example3.png'),
+    require('../../../assets/Blender/example4.png'),
+    require('../../../assets/Blender/example5.png'),
+    require('../../../assets/Blender/example6.png'),
+    require('../../../assets/Blender/example7.png'),
+    require('../../../assets/Blender/example8.png')
   ]
   return (
     <div className="LeftList">
       {fromCourse.map((item, index) => {
         return (
-      <div key={index} className='example' style={{ backgroundImage: `url(${require('../../../assets/Blender/ThreeGuys.png')})`, backgroundSize: 'cover' }}>
+      <div key={index} className='example' style={{ backgroundImage: `url(${item})`, backgroundSize: 'cover' }}>
       </div>)
       })}
     </div>
--- src/pages/Blender/RightList/index.js
+++ src/pages/Blender/RightList/index.js
@@ -2,9 +2,19 @@ import React from 'react'
 import './index.scss'
 
 const List = () => {
+  const fromNick = [
+    require('../../../assets/Blender/example9.png'),
+    require('../../../assets/Blender/example10.png'),
+    require('../../../assets/Blender/example11.png'),
+    require('../../../assets/Blender/example12.png')
+  ]
   return (
     <div className="RightList">
-      我是list
+      {fromNick.map((item, index) => {
+        return (
+      <div key={index} className='example' style={{ backgroundImage: `url(${item})`, backgroundSize: 'cover' }}>
+      </div>)
+      })}
     </div>
   )
 }
