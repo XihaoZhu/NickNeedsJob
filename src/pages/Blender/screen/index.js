b4a062139e7831aec559f028bca1aaffb3020b2f
Xihao Zhu <xihao_zhu2021@163.com>
2024-01-16 00:28:05 +0000

    waiting for the final gif

--- src/pages/Blender/screen/index.js
+++ src/pages/Blender/screen/index.js
@@ -3,12 +3,34 @@ import './index.scss'
 
 const Screen = ({ showWhich }) => {
   const list = [
-    require('../../../assets/Blender/example1.png')
+    require('../../../assets/Blender/example1.png'),
+    require('../../../assets/BlenderPage/example2.gif'),
+    require('../../../assets/BlenderPage/example3.gif'),
+    require('../../../assets/BlenderPage/example4.gif'),
+    require('../../../assets/BlenderPage/example5.gif'),
+    require('../../../assets/BlenderPage/example6.gif'),
+    require('../../../assets/BlenderPage/example7.gif'),
+    require('../../../assets/BlenderPage/example8.gif'),
+    require('../../../assets/BlenderPage/example9.png'),
+    require('../../../assets/BlenderPage/example10.png'),
+    require('../../../assets/BlenderPage/example11.png'),
+    require('../../../assets/BlenderPage/example9.png')
   ]
   return (
     <>
     <div className="Screen">
-      <img src={list[0]}></img>
+      {showWhich !== -1
+        ? <img src={list[showWhich]}></img>
+        : (
+        <div className='introduction'>
+          <h2>Some practices I have done</h2>
+          <br/>
+          <span> Here I would show you some practices I have done with blender. Unfortunately for quicker loading, files qualities here are all under compression. <br/><br/>
+            On the left hand side, there shows 8 works I produced, following instructions on Internet but also combined my own ideas. <br/><br/>
+            On right hand side, I selected 4 original works : Refer to the classic Chanel N°5; Refer to a cup I own; hair material and practice of liquid.
+          </span>
+        </div>
+          )}
     </div>
     </>
   )
